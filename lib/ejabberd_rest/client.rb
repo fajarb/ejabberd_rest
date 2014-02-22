bf40d4b22cfa006caf6a3cecda2b41c2eba4cafe
Fajar Budiprasetyo <fajar.ab@gmail.com>
2014-02-22 08:35:25 +0700

    Add PubSub delete node

--- lib/ejabberd_rest/client.rb
+++ lib/ejabberd_rest/client.rb
@@ -35,15 +35,23 @@ module EjabberdRest
       post("/rest", body: stanza)
     end
 
+    def pubsub_delete_node(from_jid, host, node)
+      stanza =  "<iq type='set' from='#{from_jid}' to='#{host}' id='#{SecureRandom.uuid}'>"
+      stanza <<   "<pubsub xmlns='http://jabber.org/protocol/pubsub#owner'>"
+      stanza <<     "<delete node='#{node}'/>"
+      stanza <<   "</pubsub>"
+      stanza << "</iq>"
+    end
+
     def pubsub_publish(from_jid, host, node, message)
       stanza =  "<iq type='set' from='#{from_jid}' to='#{host}' id='#{SecureRandom.uuid}'>"
-      stanza << "  <pubsub xmlns='http://jabber.org/protocol/pubsub'>"
-      stanza << "    <publish node='#{node}'>"
-      stanza << "      <item id='#{SecureRandom.uuid}'>"
-      stanza << "        #{message}"
-      stanza << "      </item>"
-      stanza << "    </publish>"
-      stanza << "  </pubsub>"
+      stanza <<   "<pubsub xmlns='http://jabber.org/protocol/pubsub'>"
+      stanza <<     "<publish node='#{node}'>"
+      stanza <<       "<item id='#{SecureRandom.uuid}'>"
+      stanza <<         "#{message}"
+      stanza <<       "</item>"
+      stanza <<     "</publish>"
+      stanza <<   "</pubsub>"
       stanza << "</iq>"
 
       post_stanza(stanza)
